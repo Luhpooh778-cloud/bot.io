94f00e2490d425c22d57599e758a984a80f1a99a
Luhpooh778-cloud <Luhpooh778@icloud.com>
2026-02-18 13:05:33 -0500

    requirements.txt

--- bot.py
+++ bot.py
@@ -1,18 +1,2 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-BOT_TOKEN = "8250106450:AAFkzjLhAxihUlKxPhMueaTCOY3xv9njNdM"
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is running! 🚀")
-
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-
-print("Bot running...")
-
-app.run_polling()
-Add file → Create new file
+bot.py
 requirements.txt
-python-telegram-bot==20.7
